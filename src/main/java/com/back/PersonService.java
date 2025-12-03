2379f0573abf3c62bf54a8aba64915a689a7fbb3
hhyukk <hhyukk1273@gmail.com>
2025-12-03 15:27:22 +0900

    feat: PersonRepository 다중 빈 등록 및 PersonService에 특정 빈 주입 구성

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,10 +1,14 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
+    private final PersonRepository personRepository2;
+
     public long count(){
-        return 3;
+        return personRepository2.count();
     }
 }
