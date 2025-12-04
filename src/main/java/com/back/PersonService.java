de513b3948234869c0b0060a6144bceab76276ca
hhyukk <hhyukk1273@gmail.com>
2025-12-04 09:56:43 +0900

    feat: AppConfig에 version 빈 추가하고 PersonRepository에 주입되도록 구성

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
     private final PersonRepository personRepository;
 
-    public PersonService(@Qualifier("personRepository2") PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
     public long count(){
         return personRepository.count();
     }
