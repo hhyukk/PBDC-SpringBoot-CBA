19eb2940646ed4266fa4f40aa34083e008b33e25
hhyukk <hhyukk1273@gmail.com>
2025-12-05 10:10:33 +0900

    feat: Member 도메인 및 초기 데이터 자동 생성 로직 추가

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,7 +1,6 @@
 package com.back;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     private final PersonRepository personRepository;
 
-    public long count(){
+    public long count() {
         return personRepository.count();
     }
 }
